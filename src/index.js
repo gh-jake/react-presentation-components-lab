2c42f3555597c996a701812d4b2f7ba715fb6cdc
Jake Houston <jakechouston@gmail.com>
2021-04-21 16:26:26 -0400

    commit tree 3b5934175926e9cd0d2a7ac53316358b161593b3

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+// simplerClick = () => {
+//   return <div>me too</div>
+// }
+
 ReactDOM.render(
+  
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => console.log('clicked')} />
   </div>,
   document.getElementById('root')
 );
